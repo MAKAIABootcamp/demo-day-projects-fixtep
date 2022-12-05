62e021f62e2814393e9c26cf220ee9682407d8e8
camilon0 <kmilo8433@gmail.com>
2022-12-05 17:36:39 -0500

    login modal

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import store from "./redux/store/store";
 import Router from "./routes/Router";
+import "./style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={store}>
-        <Router />
-    </Provider>
+  <Provider store={store}>
+    <Router />
+  </Provider>
 );
