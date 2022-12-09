52c361e9549d66b29d224b6c704653f60e86ddc8
camilon0 <kmilo8433@gmail.com>
2022-12-09 13:42:59 -0500

    cambios

--- src/components/contratistas/DetalleContratista.jsx
+++ src/components/contratistas/DetalleContratista.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { Form } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Modal from "react-bootstrap/Modal";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import Cita from "./Cita";
 import { time } from "../../services/data";
-import { Form } from "react-bootstrap";
+import Cita from "./Cita";
 const images = require.context("./assets/img", true);
 
 // "https://previews.123rf.com/images/dolgachov/dolgachov1610/dolgachov161012005/64860816-profesi%C3%B3n-carpinter%C3%ADa-ebanister%C3%ADa-y-el-concepto-de-la-gente-carpintero-con-tablones-de-madera-martil.jpg"
@@ -33,82 +33,74 @@ const DetalleContratista = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return (
-    <div className="details">
-      {infoWorker ? (
-        <article className="details__card">
-          <img src={infoWorker.image} />
-          <div className="info">
-            <h4>{infoWorker.profession}</h4>
-            <span>{infoWorker.name}</span>
-            <span>Experiencia : {infoWorker.expertise}</span>
-            <span>Contacto: {infoWorker.phone}</span>
-            <Button variant="primary" onClick={handleShow}>
-        Agendar cita
-      </Button>
+    <>
+      <div className="details">
+        {infoWorker ? (
+          <article className="details__card">
+            <img src={infoWorker.image} />
+            <div className="info">
+              <h4>{infoWorker.profession}</h4>
+              <span>{infoWorker.name}</span>
+              <span>Experiencia : {infoWorker.expertise}</span>
+              <span>Contacto: {infoWorker.phone}</span>
+              <Button variant="primary" onClick={handleShow}>
+                Agendar cita
+              </Button>
 
-      <Modal show={show} onHide={handleClose} animation={false}>
-        <Modal.Header closeButton>
-          <Modal.Title>Agendar cita</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-          <input type="date" />
-          <Form.Select
-                  aria-label="Default select example"
-                >
-                  <option value="">Selecciona una hora</option>
-                  {time.map((item) => (
-                    <option
-                      key={item.value}
-                      value={item.label}
-                      className="text-capitalize"
-                    >
-                      {item.label}
-                    </option>
-                  ))}
-                </Form.Select>
-                <label>Nombre
-                <input type="text"/>
-                </label>
-                <label>Telefono
-                <input type="text"/>
-                </label>
-                <label>Dirección
-                <input type="text"/>
-                </label>
-                </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Cerrar
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Guardar
-          </Button>
-        </Modal.Footer>
-      </Modal>
-          </div>
-        </article>
-      ) : (
-        <div>no haz seleccionado un contratista</div>
-      )}
+              <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Agendar cita</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form>
+                    <input type="date" />
+                    <Form.Select aria-label="Default select example">
+                      <option value="">Selecciona una hora</option>
+                      {time.map((item) => (
+                        <option
+                          key={item.value}
+                          value={item.label}
+                          className="text-capitalize"
+                        >
+                          {item.label}
+                        </option>
+                      ))}
+                    </Form.Select>
+                    <input></input>
+                  </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Cerrar
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                    Guardar cambios
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </div>
+          </article>
+        ) : (
+          <div>no haz seleccionado un contratista</div>
+        )}
 
-      <div className="details__work">
-        <Card style={{ width: "14rem", height: "50%" }}>
-          <Card.Img variant="top" src={images(`./trabajo1.jpg`)} />
-        </Card>
-        <Card style={{ width: "14rem", height: "50%" }}>
-          <Card.Img variant="top" src={images(`./trabajo2.jpg`)} />
-        </Card>
-        <Card style={{ width: "14rem", height: "50%" }}>
-          <Card.Img variant="top" src={images(`./trabajo3.jpg`)} />
-        </Card>
-        <Card style={{ width: "14rem", height: "50%" }}>
-          <Card.Img variant="top" src={images(`./trabajo4.jpg`)} />
-        </Card>
+        <div className="details__work">
+          <Card style={{ width: "14rem", height: "50%" }}>
+            <Card.Img variant="top" src={images(`./trabajo1.jpg`)} />
+          </Card>
+          <Card style={{ width: "14rem", height: "50%" }}>
+            <Card.Img variant="top" src={images(`./trabajo2.jpg`)} />
+          </Card>
+          <Card style={{ width: "14rem", height: "50%" }}>
+            <Card.Img variant="top" src={images(`./trabajo3.jpg`)} />
+          </Card>
+          <Card style={{ width: "14rem", height: "50%" }}>
+            <Card.Img variant="top" src={images(`./trabajo4.jpg`)} />
+          </Card>
+        </div>
+        <Cita />
       </div>
-      <Cita />
-    </div>
+    </>
   );
 };
 
